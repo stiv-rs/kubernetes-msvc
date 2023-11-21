a686de6600d5097c9650c6e93e4da18e3f631b84
Stiv <strodriguezna@gmail.com>
2023-11-21 22:26:38 +0100

    Probando el LOADBALANCER con los datos del POD en los objetos

--- msvc-usuarios/src/main/java/com/stiv/msvc/usuarios/controllers/UsuarioController.java
+++ msvc-usuarios/src/main/java/com/stiv/msvc/usuarios/controllers/UsuarioController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -23,14 +24,22 @@ public class UsuarioController {
     @Autowired
     private ApplicationContext context;
 
+    @Autowired
+    private Environment env;
+
     @GetMapping("/crash")
     public void crash(){
         ((ConfigurableApplicationContext)context).close();
     }
 
     @GetMapping
-    public Map<String, List<Usuario>> listar(){
-        return Collections.singletonMap("Usuarios", service.listar());
+    public ResponseEntity<?> listar(){
+        Map<String, Object> body = new HashMap<>();
+        body.put("Usuarios", service.listar());
+        body.put("podinfo", env.getProperty("MY_POD_NAME")+" : "+ env.getProperty("MY_POD_IP"));
+
+        //return Collections.singletonMap("Usuarios", service.listar());
+        return  ResponseEntity.ok(body);
     }
 
     @GetMapping("/{id}")
