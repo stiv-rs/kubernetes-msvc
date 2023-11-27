bf6d2ada9e189a08439e81e050f2847de85edb85
Stiv <strodriguezna@gmail.com>
2023-11-27 18:44:38 +0100

    Configurando el OAuth Resource server para proteger las rutas

--- msvc-usuarios/src/main/java/com/stiv/msvc/usuarios/SecurityConfig.java
+++ msvc-usuarios/src/main/java/com/stiv/msvc/usuarios/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.stiv.msvc.usuarios;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,12 +13,19 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http.authorizeRequests().anyRequest().authenticated()
+        http.authorizeRequests()
+                .requestMatchers("/autorized").permitAll()
+                .requestMatchers(HttpMethod.GET,"/", "/{id}").hasAnyAuthority("SCOPE_read", "SCOPE_write")
+                .requestMatchers(HttpMethod.POST,"/").hasAuthority("SCOPE_write")
+                .requestMatchers(HttpMethod.PUT,"/","/{id}").hasAuthority("SCOPE_write")
+                .requestMatchers(HttpMethod.DELETE,"/","/{id}").hasAuthority("SCOPE_write")
+                .anyRequest().authenticated()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .oauth2Login(oauth2Login -> oauth2Login.loginPage("/oauth2/authorization/msvc-usuarios-client"))
-                .oauth2Client(withDefaults());
+                .oauth2Client(withDefaults())
+                .oauth2ResourceServer().jwt();
         return http.build();
     }
 }
